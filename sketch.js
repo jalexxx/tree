b8813b74f4a4a5121ab05427996e2f1369fbf0db
jalexxx <70824015+jalexxx@users.noreply.github.com>
2021-06-29 22:59:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,5 +72,3 @@ function branch(b_length, theta)
 
 }
 
-/* I have attempted the recursive pattern tree assingment, I attempted in making the tree change numerous colours as well added the animatuon of one of the tree's split, moving in circular motion  and gathering together to 'form of the tree' almost like showing how the first and main tree was made. My code managed to do so as the tree flashes different 3 colours and creates an illuminating colour. The broken tree pieces also moves outer part of canvas to the centre. I was able to achieve the animated part due to p5.Vector.fromAngle and colours were made in variales then implemented in the stroke as normal colours. I am satifised with how it looks overall as it looks somewhat 3d, however I would like to make more trees next time with similar colour effect however slow down the trasnition between the colours as it's too fast and it's hard to distinguish which colours were used. */ 
-
